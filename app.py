0652305e0b04e2975e089c2bbe04299d37adfa66
Aishwarya160 <aishwarya161005@gmail.com>
2026-01-01 11:10:18 +0530

    Modifications

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ def utc_to_ist(dt):
 app = Flask(__name__)
 app.config.update(
     SESSION_COOKIE_SAMESITE="Lax",
-    SESSION_COOKIE_SECURE=True
+    SESSION_COOKIE_SECURE=True,
     SESSION_COOKIE_HTTPONLY=True
 )
 
